c7ca2d1087f9ee0f3c4652a60380e418e0c8055b
ChenHaoJie <15839177136@163.com>
2022-03-09 19:25:15 +0800

    制造冲突2

--- hello/src/com/chj/Hello.java
+++ hello/src/com/chj/Hello.java
@@ -3,9 +3,9 @@ package com.chj;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("asdasd");
-		System.out.println("asdasd捱三顶五撒大");
-		System.out.println("李文昊月薪100万");
-		System.out.println("葛中响月薪100元");
+		System.out.println("asdasd捱三asdas顶五撒大");
+		System.out.println("李文昊月asdasd薪100万");
+		System.out.println("葛中响月dasdsad薪100元");
 		System.out.println("asdasd捱三顶五撒大");
     }
 }
