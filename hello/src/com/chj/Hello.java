1a4cd3c324cda4fbb2160f59ae25bf5234ead54c
ChenHaoJie <15839177136@163.com>
2022-03-09 19:03:06 +0800

    我更改了消息

--- hello/src/com/chj/Hello.java
+++ hello/src/com/chj/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("asdasd");
 		System.out.println("asdasd捱三顶五撒大");
+		System.out.println("李文昊月薪100万");
+		System.out.println("葛中响月薪100元");
 		System.out.println("asdasd捱三顶五撒大");
     }
 }
