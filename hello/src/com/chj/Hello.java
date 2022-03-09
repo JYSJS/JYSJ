a92b6cffe9156c07719bb539467c864b75cf9947
ChenHaoJie <15839177136@163.com>
2022-03-09 19:54:57 +0800

    阿萨伟大时代

--- hello/src/com/chj/Hello.java
+++ hello/src/com/chj/Hello.java
@@ -8,13 +8,5 @@ public class Hello {
 		System.out.println("葛中响月dasdsad薪100元");
 		System.out.println("asdasd捱三顶五撒大1");
 		System.out.println("葛中响月薪100元a");
-<<<<<<< HEAD
-		System.out.println("asdasd捱三顶五撒大");
-		System.out.println("asdasd捱三顶五撒大aaa");
-		
-=======
-		System.out.println("asdasd捱三顶五撒大更好的地方给对方");
-		System.out.println("asdasd捱三顶五撒大更好的地方给对阿达的方");
->>>>>>> d2a735e24fe0b5ff526aec6cda6e177fb361859b
     }
 }
