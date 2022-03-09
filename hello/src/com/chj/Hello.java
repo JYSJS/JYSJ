d2a735e24fe0b5ff526aec6cda6e177fb361859b
ChenHaoJie <15839177136@163.com>
2022-03-09 19:48:08 +0800

    制造冲突

--- hello/src/com/chj/Hello.java
+++ hello/src/com/chj/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 		System.out.println("葛中响月dasdsad薪100元");
 		System.out.println("asdasd捱三顶五撒大1");
 		System.out.println("葛中响月薪100元a");
-		System.out.println("asdasd捱三顶五撒大");
+		System.out.println("asdasd捱三顶五撒大更好的地方给对方");
+		System.out.println("asdasd捱三顶五撒大更好的地方给对阿达的方");
     }
 }
